c128c1077db77ff986ec5558bce5ed9148cd820b
Bala05104 <77102143+Bala05104@users.noreply.github.com>
2021-02-08 14:26:36 +0530

    cloud storage
    cloud storage in aws platform

---
+++ CNN+RNN/s3 storage.py
@@ -0,0 +1,29 @@
+import boto3
+import pandas
+# Creating the low level functional client
+client = boto3.client(
+    's3',
+    aws_access_key_id = 'AKIAI7M5MGZEAWGEXLNA',
+    aws_secret_access_key = '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',
+    region_name = 'us-east-2'
+)
+# Fetch the list of existing buckets
+clientResponse = client.list_buckets()
+    
+# Print the bucket names one by one
+print('Printing bucket names...')
+for bucket in clientResponse['Buckets']:
+    print(f'Bucket Name: {bucket["Name"]}')
+
+# Create the S3 object
+obj = client.get_object(
+    Bucket = 'myevaluate12',
+    Key = 'test.csv'
+)
+    
+# Read data from the S3 object
+data = pandas.read_csv(obj['Body'])
+    
+# Print the data frame
+print('Printing the data frame...')
+print(data.head(10))
